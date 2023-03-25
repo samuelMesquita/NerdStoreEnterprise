30c7b5f0a4da31e8d7844ef9db2061c4b6e150a3
Samuel Mesquita da Fonseca <75632669+samuelMesquita@users.noreply.github.com>
2023-03-25 03:28:45 -0300

    feat implementação do swagger e das migrations

---
+++ src/services/identidade/NSE.Identidade.API/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API
+{
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager = null)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest(usuarioRegistro);
+
+            var user = new IdentityUser {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if(!ModelState.IsValid) return BadRequest(usuarioLogin);
+
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
+        
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            else if (result.IsLockedOut)
+            {
+                return BadRequest("Numero de tentativas exedidas");
+            }
+
+            return BadRequest();
+        }
+    }
+}
--- src/services/identidade/NSE.Identidade.API/Startup.cs
+++ src/services/identidade/NSE.Identidade.API/Startup.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using NSE.Identidade.API.Data;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NSE.Identidade.API
 {
@@ -23,7 +23,39 @@ namespace NSE.Identidade.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnections")
+                    )
+                );
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddRazorPages();
+
+            services.AddSwaggerGen(c => 
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Nerd Story Enteprise Identity Api",
+                    Description = "Essa Api faz parte do curso ASP.Net Core Enterprise",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Samuel Mesquita da Fonseca",
+                        Email = "samuelmesquita.df@gmail.com",
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                })
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +64,12 @@ namespace NSE.Identidade.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
             }
             else
             {
@@ -45,6 +83,7 @@ namespace NSE.Identidade.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
