c4b4e90919561667497804c6c6f8242acfd88a71
Samuel Mesquita da Fonseca <75632669+samuelMesquita@users.noreply.github.com>
2023-04-26 00:18:41 -0300

    feat alterações no projeto web

---
+++ src/web/MVC/NSE.WebApp.Mvc/Controllers/IdentidadeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.Mvc.Models;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.Mvc.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("criar-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("criar-conta")]
+        public Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+
+        }
+    }
+}
